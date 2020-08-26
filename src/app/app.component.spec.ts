3f19fd69bc8d00f1aa9c2e3869493efb3b5474a1
emclaug2 <evan.w.mclaughlin@gmail.com>
2020-08-26 13:24:34 -0400

    Run prettier

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,11 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {AppModule} from "./app.module";
+import { AppModule } from './app.module';
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
         void TestBed.configureTestingModule({
-            imports: [
-                AppModule
-            ]
+            imports: [AppModule],
         }).compileComponents();
     }));
 
