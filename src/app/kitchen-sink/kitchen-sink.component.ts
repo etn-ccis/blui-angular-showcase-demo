ad19b5941b74a0e88cf0a1da097fbcf35b2109e4
Firm-Soft <evan.w.mclaughlin@gmail.com>
2021-02-08 09:15:11 -0500

    Invert some icons, match storybook styles

--- src/app/kitchen-sink/kitchen-sink.component.ts
+++ src/app/kitchen-sink/kitchen-sink.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import * as PXBColors from '@pxblue/colors';
 
 @Component({
     selector: 'app-kitchen-sink',
     templateUrl: './kitchen-sink.component.html',
     styleUrls: ['./kitchen-sink.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class KitchenSinkComponent {
     colors = PXBColors;
