84f3f94ba742babc7f68f7a32bee9d31b7284e9d
emclaug2 <evan.w.mclaughlin@gmail.com>
2021-08-12 13:10:05 -0400

    Run prettier

--- src/app/drawer/drawer.component.ts
+++ src/app/drawer/drawer.component.ts
@@ -3,7 +3,7 @@ import * as PXBColors from '@pxblue/colors';
 import { DrawerNavItem } from '@pxblue/angular-components';
 import { ViewportService } from '../services/viewport.service';
 import { StateService } from '../services/state.service';
-import {NavigationEnd, Router} from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
     selector: 'showcase-drawer',
@@ -66,57 +66,57 @@ export class DrawerComponent {
             if (route instanceof NavigationEnd && !this.selectedItemId) {
                 switch (route.urlAfterRedirects.split('?')[0]) {
                     case `/pxblue-components/data-display-components`: {
-                        this.selectedItemId = `${this.pxblueGroupId}${this.dataDisplay}`
+                        this.selectedItemId = `${this.pxblueGroupId}${this.dataDisplay}`;
                         this.navItems[0].expanded = true;
                         break;
                     }
                     case `/pxblue-components/navigation-components`: {
-                        this.selectedItemId = `${this.pxblueGroupId}${this.navigation}`
+                        this.selectedItemId = `${this.pxblueGroupId}${this.navigation}`;
                         this.navItems[0].expanded = true;
                         break;
                     }
                     case `/pxblue-components/surface-components`: {
-                        this.selectedItemId = `${this.pxblueGroupId}${this.surfaces}`
+                        this.selectedItemId = `${this.pxblueGroupId}${this.surfaces}`;
                         this.navItems[0].expanded = true;
                         break;
                     }
                     case `/material-components/data-display-components`: {
-                        this.selectedItemId = `${this.materialComponentsGroupId}${this.dataDisplay}`
+                        this.selectedItemId = `${this.materialComponentsGroupId}${this.dataDisplay}`;
                         this.navItems[1].expanded = true;
                         break;
                     }
                     case `/material-components/feedback-components`: {
-                        this.selectedItemId = `${this.materialComponentsGroupId}${this.feedback}`
+                        this.selectedItemId = `${this.materialComponentsGroupId}${this.feedback}`;
                         this.navItems[1].expanded = true;
                         break;
                     }
                     case `/material-components/input-components`: {
-                        this.selectedItemId = `${this.materialComponentsGroupId}${this.inputs}`
+                        this.selectedItemId = `${this.materialComponentsGroupId}${this.inputs}`;
                         this.navItems[1].expanded = true;
                         break;
                     }
                     case `/material-components/navigation-components`: {
-                        this.selectedItemId = `${this.materialComponentsGroupId}${this.navigation}`
+                        this.selectedItemId = `${this.materialComponentsGroupId}${this.navigation}`;
                         this.navItems[1].expanded = true;
                         break;
                     }
                     case `/material-components/surface-components`: {
-                        this.selectedItemId = `${this.materialComponentsGroupId}${this.surfaces}`
+                        this.selectedItemId = `${this.materialComponentsGroupId}${this.surfaces}`;
                         this.navItems[1].expanded = true;
                         break;
                     }
                     case `/templates/alarms`: {
-                        this.selectedItemId = `${this.pageTemplatesGroupId}${this.alarms}`
+                        this.selectedItemId = `${this.pageTemplatesGroupId}${this.alarms}`;
                         this.navItems[2].expanded = true;
                         break;
                     }
                     case `/templates/settings`: {
-                        this.selectedItemId = `${this.pageTemplatesGroupId}${this.settings}`
+                        this.selectedItemId = `${this.pageTemplatesGroupId}${this.settings}`;
                         this.navItems[2].expanded = true;
                         break;
                     }
                     case `/templates/dashboard`: {
-                        this.selectedItemId = `${this.pageTemplatesGroupId}${this.dashboard}`
+                        this.selectedItemId = `${this.pageTemplatesGroupId}${this.dashboard}`;
                         this.navItems[2].expanded = true;
                         break;
                     }
