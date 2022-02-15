8ff4c58644a847b7f8d7ee40635e77f85966f754
emclaug2 <evan.w.mclaughlin@gmail.com>
2022-02-15 14:20:21 -0500

    Update typogaphy rules

--- src/app/pages/blui/typography.component.ts
+++ src/app/pages/blui/typography.component.ts
@@ -4,79 +4,93 @@ import {Component} from "@angular/core";
     selector: 'app-blui-typography',
     template: `
         <div style="padding: 16px">
-            <div class="title">Display 4 / Headline 1</div>
-            <div class="typography-line">
-                <div class="mat-display-4">Mat</div>
-                <div class="blui-headline-1">Blui</div>
-            </div>
-            <div class="title">Display 3 / Headline 2</div>
-            <div class="typography-line">
-                <div class="mat-display-3">Mat</div>
-                <div class="blui-headline-2">Blui</div>
-            </div>
-            <div class="title">Display 2 / Headline 3</div>
-            <div class="typography-line">
-                <div class="mat-display-2">Mat</div>
-                <div class="blui-headline-3">Blui</div>
-            </div>
-            <div class="title">Display 1 / Headline 4</div>
-            <div class="typography-line">
-                <div class="mat-display-1">Mat</div>
-                <div class="blui-headline-4">Blui</div>
-            </div>
-            <div class="title">Headline / Headline-5</div>
-            <div class="typography-line">
-                <div class="mat-headline">Mat</div>
-                <div class="blui-headline-5">Blui</div>
-            </div>
-            <div class="title">Title / Headline 1</div>
-            <div class="typography-line">
-                <div class="mat-title">Mat</div>
-                <div class="blui-headline-6">Blui</div>
-            </div>
-            <div class="title">Subheading 2 / Subtitle 1</div>
-            <div class="typography-line">
-                <div class="mat-subheading-2">Mat</div>
-                <div class="blui-subtitle-1">Blui</div>
-            </div>
-            <div class="title">Subheading 1 / Subtitle 2</div>
-            <div class="typography-line">
-                <div class="mat-subheading-1">Mat</div>
-                <div class="blui-subtitle-2">Blui</div>
-            </div>
-            <div class="title">Body 1 / Body 1</div>
-            <div class="typography-line">
-                <div class="mat-body-1">Mat</div>
-                <div class="blui-body-1">Blui</div>
-            </div>
-            <div class="title">Body 2 / Body 2</div>
-            <div class="typography-line">
-                <div class="mat-body-2">Mat</div>
-                <div class="blui-body-2">Blui</div>
-            </div>
-            <div class="title">Subheading2 / Subtitle 2</div>
-            <div class="typography-line">
-                <div class="mat-subheading-2">Mat</div>
-                <div class="blui-subtitle-2">Blui</div>
-            </div>
-            <div class="title">Caption</div>
-            <div class="typography-line">
-                <div class="mat-caption">Mat</div>
-                <div class="blui-caption">Blui</div>
-            </div>
-            <div class="title">Button / Button </div>
-            <div class="typography-line">
-                <div class="mat-button">Mat</div>
-                <div class="blui-button">Blui</div>
-            </div>
-            <div class="title">Input</div>
-            <div class="typography-line">
-                <div class="mat-input">Mat</div>
-            </div>
-            <div class="title">Caption</div>
-            <div class="typography-line">
-                <div class="blui-caption">Blui</div>
-            </div>
+            <table id="type-table">
+                <tr>
+                    <th>Mat Name</th>
+                    <th></th>
+                    <th></th>
+                    <th>BLUI Name</th>
+                </tr>
+                <tr>
+                    <td>Display 4</td>
+                    <td><span class="mat-display-4">Test</span></td>
+                    <td><span class="blui-headline-1">Test</span></td>
+                    <td>Headline 1</td>
+                </tr>
+                <tr>
+                    <td>Display 3</td>
+                    <td><span class="mat-display-3">Test</span></td>
+                    <td><span class="blui-headline-2">Test</span></td>
+                    <td>Headline 2</td>
+                </tr>
+                <tr>
+                    <td>Display 2</td>
+                    <td><span class="mat-display-2">Test</span></td>
+                    <td><span class="blui-headline-3">Test</span></td>
+                    <td>Headline 3</td>
+                </tr>
+                <tr>
+                    <td>Display 1</td>
+                    <td><span class="mat-display-1">Test</span></td>
+                    <td><span class="blui-headline-4">Test</span></td>
+                    <td>Headline 4</td>
+                </tr>
+
+                <tr>
+                    <td>Headline</td>
+                    <td><span class="mat-headline">Test</span></td>
+                    <td><span class="blui-headline-5">Test</span></td>
+                    <td>Headline 5</td>
+                </tr>
+                <tr>
+                    <td>Title</td>
+                    <td><span class="mat-title">Test</span></td>
+                    <td><span class="blui-headline-6">Test</span></td>
+                    <td>Headline 6</td>
+                </tr>
+                <tr>
+                    <td>Subheading 2</td>
+                    <td><span class="mat-subheading-2">Test</span></td>
+                    <td><span class="blui-subtitle-2">Test</span></td>
+                    <td>Subtitle 2</td>
+                </tr>
+                <tr>
+                    <td>Subheading 1</td>
+                    <td><span class="mat-subheading-1">Test</span></td>
+                    <td><span class="blui-subtitle-1">Test</span></td>
+                    <td>Subtitle 1</td>
+                </tr>
+                <tr>
+                    <td>Body 1</td>
+                    <td><span class="mat-body-1">Test</span></td>
+                    <td><span class="blui-body-1">Test</span></td>
+                    <td>Body 1</td>
+                </tr>
+                <tr>
+                    <td>Body 2</td>
+                    <td><span class="mat-body-2">Test</span></td>
+                    <td><span class="blui-body-2">Test</span></td>
+                    <td>Body 2</td>
+                </tr>
+                <tr>
+                    <td>Caption</td>
+                    <td><span class="mat-caption">Test</span></td>
+                    <td><span class="blui-caption">Test</span></td>
+                    <td>Caption</td>
+                </tr>
+                <tr>
+                    <td>Button</td>
+                    <td><span class="mat-button">Test</span></td>
+                    <td><span class="blui-button">Test</span></td>
+                    <td>Button</td>
+                </tr>
+                <tr>
+                    <td>Overline</td>
+                    <td><span class="mat-overline">Test</span></td>
+                    <td><span class="blui-overline">Test</span></td>
+                    <td>Overline</td>
+                </tr>
+            </table>
         </div>
     `,
     styles: [
